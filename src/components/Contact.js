07f37afd059ab9c513d0fc3a62caf4dd406493cf
Joachim <47396357+jokkejok123@users.noreply.github.com>
2019-10-27 00:00:00 +0200

    Added linkedin link. Changed current tab indicator

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,8 +8,9 @@ export default class Contact extends Component {
                     <h1 className="heading grid-lg-6 grid-xs-12 grid-sm-8 grid-md-6">Contact info</h1>
                     </div>
                     <div className="row">
-                    <p className="lg-pg grid-lg-6 grid-xs-12 grid-sm-8 grid-md-6">Should anything have caught your interest feel free to contact me using the address below:<br /><br />
-                        <a className="intext" href>joac1099@gmail.com</a></p>
+                        <p className="lg-pg grid-lg-6 grid-xs-12 grid-sm-8 grid-md-6">Should anything have caught your interest feel free to contact me using the following below:<br /><br />
+                        Email: <a className="intext" href="mailto:joac1099@gmail.com">joac1099@gmail.com</a><br/><br/>
+                        <a className="intext" href="https://www.linkedin.com/in/joachim-hviid/">LinkedIn</a></p>
                     </div>
             </main>
         )
